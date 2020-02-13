de222df0618ed34f313c386cfaae689c5d8786af
w-arantes <w.aranteslfc@gmail.com>
2020-02-13 00:38:13 -0300

    chore: gobarber-api first commit

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9e5c07f5be4a0c',
+    pass: 'dfef8c43a493f4',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
