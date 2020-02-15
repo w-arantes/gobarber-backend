7e220cc2d304dfbb0bdf68bc3dd5314ca2626d25
w-arantes <w.aranteslfc@gmail.com>
2020-02-15 00:46:00 -0300

    feature: gobarber-backend-complete

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '36828012b7696ab64196e0f636018a2e',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '9e5c07f5be4a0c',
-    pass: 'dfef8c43a493f4',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
